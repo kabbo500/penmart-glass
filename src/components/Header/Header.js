66a6840b2797169f86189db80cbf9c75b4e6fe2a
kabbo500 <kabbokazi3779@gmail.com>
2022-04-04 11:21:47 +0600

    add nav bar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+import "./Header.css"
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav>
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/reviews">Reviews</CustomLink>
+                <CustomLink to="/dashboard">Dashboard</CustomLink>
+                <CustomLink to="/blogs">Blogs</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+            </nav>
+            <h1>Welcome to Lenscart Bangladesh</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Notfound/Notefound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notefound = () => {
+    return (
+        <div>
+            <h5>The page you looking for is not available</h5>
+            <h1>404!</h1>
+        </div>
+    );
+};
+
+export default Notefound;
