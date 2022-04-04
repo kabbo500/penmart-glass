efa85b2b22fdeb8203f99a916003399243621c7c
kabbo500 <kabbokazi3779@gmail.com>
2022-04-04 11:47:04 +0600

    add home article

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            <p>this is home</p>
+        <div className='homepage-container'>
+            <div className="article-part">
+                <h1>Your next glass <br />
+                    Your best glass
+                </h1>
+                <p>Sunglasses are designed to protect the eyes both from excessive light and from damaging UV light rays.Sunglasses are aimed at the young but they are more than a fashion statement, they play an essential role in protecting the eyes.</p>
+            </div>
+            <div className="img-part">
+                <img src="https://www.linkpicture.com/q/a5a7a48b62ad37bb168c4e9a217bf781_1.jpg" alt="" />
+            </div>
         </div>
     );
 };
